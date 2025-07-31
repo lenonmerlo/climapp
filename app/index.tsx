7184b273959cf2ac7044227b3738fd328cbe07e6
lenonmerlo <lenontm@gmail.com>
2025-07-31 09:12:48 -0300

    Implementa tela inicial com fundo gradiente e elementos visuais

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <LinearGradient colors={["#00457d", "#05051f"]} style={style.container}>
+      <Image source={require("../assets/images/Logo/Logo.png")} />
+      <Image source={require("../assets/images/weather.png")} />
+      <Text>Boas-vindas!</Text>
+      <TouchableOpacity>
+        <Text>Entrar</Text>
+      </TouchableOpacity>
+    </LinearGradient>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  texto: {
+    color: "#fff",
+    fontSize: 20,
+  },
+});
