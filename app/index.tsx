5d69a0b4cf69921174373e26d56e7ad679b9faf5
lenonmerlo <lenontm@gmail.com>
2025-07-31 09:22:27 -0300

    style: estiliza tela inicial com título, botão e ícone

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons"
 import React from "react";
 import { Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
@@ -7,9 +8,10 @@ export default function Index() {
     <LinearGradient colors={["#00457d", "#05051f"]} style={style.container}>
       <Image source={require("../assets/images/Logo/Logo.png")} />
       <Image source={require("../assets/images/weather.png")} />
-      <Text>Boas-vindas!</Text>
-      <TouchableOpacity>
-        <Text>Entrar</Text>
+      <Text style={style.title}>Boas-vindas!</Text>
+      <TouchableOpacity style={style.button}>
+        <Text style={style.buttonTitle}>Entrar</Text>
+        <MaterialIcons name="arrow-forward" size={24} color={"#01080e"}/>
       </TouchableOpacity>
     </LinearGradient>
   );
@@ -18,9 +20,32 @@ export default function Index() {
 const style = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 64,
+    paddingVertical: 79,
+    paddingHorizontal: 32,
   },
-  texto: {
+
+  title: {
     color: "#fff",
-    fontSize: 20,
+    fontSize: 25,
   },
+
+  button: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#7693ff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 32,
+    flexDirection: "row",
+    gap: 8
+  },
+
+  buttonTitle: { 
+    color: "#01080e",
+    fontSize: 20,
+    fontWeight: "600",
+  }
 });
