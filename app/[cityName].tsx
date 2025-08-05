aad79e26997a8d0045acc1489535e2bc1c8f7483
lenonmerlo <lenontm@gmail.com>
2025-08-05 16:27:54 -0300

    feat: finaliza exibição de dados climáticos com ícones e textos informativos

--- app/[cityName].tsx
+++ app/[cityName].tsx
@@ -1,10 +1,11 @@
-import { Montserrat_500Medium } from "@expo-google-fonts/montserrat";
-import { LinearGradient } from "expo-linear-gradient";
-import { useLocalSearchParams } from "expo-router";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 const CityDetails = () => {
+  const router = useRouter();
   const searchParams = useLocalSearchParams();
   const [cityDetails, setCityDetails] = useState(null);
 
@@ -13,15 +14,12 @@ const CityDetails = () => {
       const response = await fetch("https://climapp-api.vercel.app/api");
       const responseJSON = await response.json();
 
-      const paramName = Array.isArray(searchParams.cityName)
-        ? searchParams.cityName[0]
-        : searchParams.cityName;
-
       const city = responseJSON.find(
-        (cityData) => cityData.city === paramName
+        (cityData: { city: string | string[] }) =>
+          cityData.city === searchParams.cityName
       );
 
-      setCityDetails(city || {}); // evita que permaneça null
+      setCityDetails(city);
     } catch (e) {
       console.log(e);
     }
@@ -31,20 +29,65 @@ const CityDetails = () => {
     handleData();
   }, []);
 
-  if (!cityDetails || !cityDetails.city) {
+  if (!cityDetails) {
     return (
-      <LinearGradient colors={["#00457d", "#05051f"]} style={style.container}>
-        <View style={style.headerContainer}>
-          <Text style={style.headerTitle}>Carregando...</Text>
-        </View>
-      </LinearGradient>
+      <LinearGradient colors={["#00457d", "#05051f"]} style={style.container} />
     );
   }
 
   return (
     <LinearGradient colors={["#00457d", "#05051f"]} style={style.container}>
-      <View style={style.headerContainer}>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            router.back();
+          }}
+          style={style.headerIcon}
+        >
+          <MaterialIcons name="chevron-left" size={24} color={"#fff"} />
+        </TouchableOpacity>
         <Text style={style.headerTitle}>{cityDetails.city}</Text>
+      </View>
+
+      <View style={style.card}>
+        <View style={style.cardHeader}>
+          <Text style={style.cardHeaderTitle}>Hoje</Text>
+          <Text style={style.cardHeaderTitle}>{cityDetails.date}</Text>
+        </View>
+
+        <View style={style.cardBox}>
+          <Image
+            source={require("../assets/images/Ícones/Vector.png")}
+            style={style.cardImage}
+          />
+
+          <View>
+            <Text style={style.cardTemperature}>{cityDetails.temp}°</Text>
+            <Text style={style.cardDescription}>{cityDetails.description}</Text>
+          </View>
+        </View>
+
+        <View style={style.rowBox}>
+          <View style={style.row}>
+            <Image source={require("../assets/images/Ícones/humidity.png")} />
+
+            <Text style={style.rowTitle}>Humidity:</Text>
+
+            <Text style={style.rowValue}>{cityDetails.humidity}%</Text>
+          </View>
+
+          <View style={style.row}>
+            <Image
+              source={require("../assets/images/Ícones/temperature.png")}
+            />
+
+            <Text style={style.rowTitle}>Min/Max:</Text>
+
+            <Text style={style.rowValue}>
+              {cityDetails.forecast[0].min}/{cityDetails.forecast[0].max}
+            </Text>
+          </View>
+        </View>
       </View>
     </LinearGradient>
   );
@@ -55,19 +98,80 @@ const style = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 16,
     paddingTop: 40,
+    gap: 40,
   },
-
   headerContainer: {
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
   },
-
   headerTitle: {
-    color: "#FFF",
+    color: "#fff",
     fontSize: 20,
-    fontFamily: "Montserrat_500Medium",
+    fontFamily: "Montserrat_600SemiBold",
     textAlign: "center",
+  },
+  card: {
+    width: "100%",
+    borderRadius: 24,
+    backgroundColor: "#4463D5",
+    padding: 16,
+    gap: 24,
+  },
+  cardHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  cardHeaderTitle: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_600SemiBold",
+  },
+  headerIcon: {
+    position: "absolute",
+    left: 0,
+    zIndex: 10,
+  },
+  cardImage: {
+    width: 72,
+    height: 64,
+  },
+  cardTemperature: {
+    color: "#fff",
+    fontSize: 43,
+    fontFamily: "Montserrat_700Bold",
+    textAlign: "center",
+  },
+  cardDescription: {
+    color: "#fff",
+    fontSize: 13,
+    fontFamily: "Montserrat_400Regular",
+    textAlign: "center",
+  },
+  cardBox: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  rowTitle: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_600SemiBold",
+  },
+  rowValue: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_400Regular",
+    marginLeft: "auto",
+  },
+  rowBox: {
+    gap: 8,
   },
 });
 
