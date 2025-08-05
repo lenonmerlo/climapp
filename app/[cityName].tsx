e5d23bb23bde33c58025cf37b1d72d67bf30ba04
lenonmerlo <lenontm@gmail.com>
2025-08-05 10:31:51 -0300

    feat: integra tela de cidade com API externa usando fetch

--- app/[cityName].tsx
+++ app/[cityName].tsx
@@ -1,9 +1,25 @@
 import { useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
 import { View } from "react-native";
 
 const CityDetails = () => {
-    const searchParams = useLocalSearchParams()
-    console.log(searchParams)
+  const searchParams = useLocalSearchParams();
+
+  const handleData = async () => {
+    try {
+      const response = await fetch("https://climapp-api.vercel.app/api");
+
+      const responseJSON = await response.json();
+      console.log(responseJSON);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
   return <View></View>;
 };
 
